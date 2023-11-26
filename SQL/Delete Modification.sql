8489b3ff3a425fac64995335e2ee2b852e91aa9c
Mijgon <120673510+MZ-21@users.noreply.github.com>
2023-11-26 18:41:00 -0500

    Add files via upload

---
+++ SQL/Delete Modification.sql
@@ -0,0 +1,5 @@
+DELETE FROM checkoutmovie
+WHERE  email = "brittain2y@merriam-webster.com" && movieID in ("107M","307M");
+
+-- INSERT INTO checkoutmovie
+-- VALUES("2021-05-22","7:20 PM",0,"342M","aerrey12@sina.com.cn")
